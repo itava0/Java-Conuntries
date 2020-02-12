ce0d5134e2a324cceb331b0013330da9b23510e8
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-02-12 11:27:06 -0500

    Update the README file

--- src/main/java/com/countries/CountryController.java
+++ src/main/java/com/countries/CountryController.java
@@ -4,19 +4,34 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/data")
 public class CountryController {
 
-    //localHost:8080/data/names/all
+    //localhost:8080/data/names/all
 
     @GetMapping(value = "/names/all",
                 produces = {"application/json"})
     public ResponseEntity<?> getAllCountries() {
         CountrySearchApplication.ourCountryList.countryList.sort((e1, e2) -> e1.getCountryName().compareToIgnoreCase(e2.getCountryName()));
         return new ResponseEntity<>(CountrySearchApplication.ourCountryList.countryList, HttpStatus.OK);
+    }
+
+    //localhost:8080/data/names/start/{letter}
+
+    @GetMapping(value = "/names/start/{letter}",
+                produces = {"application/json"})
+
+    public ResponseEntity<?> getCounties(
+            @PathVariable
+                    char letter) {
+        ArrayList<Country> rtnCountry = CountrySearchApplication.ourCountryList.findCountry(e -> e.getCountryName().toUpperCase().charAt(0) == Character.toUpperCase(letter));
+        return new ResponseEntity<>(rtnCountry, HttpStatus.OK);
     }
 }
